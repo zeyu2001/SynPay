0627da32e75546693f79b02cf939737e605ecc4c
Zeyu Zhang <39144422+zeyu2001@users.noreply.github.com>
2024-07-04 02:03:44 +0800

    feat: example

--- examples/openapi_chain.py
+++ examples/openapi_chain.py
@@ -1,16 +1,54 @@
 from langchain.chains.openai_functions.openapi import get_openapi_chain
 from langchain_openai import ChatOpenAI
+from langchain.chains.summarize import load_summarize_chain
+from langchain.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.document_loaders import UnstructuredURLLoader
 
 llm = ChatOpenAI(model="gpt-4o")
 
-chain = get_openapi_chain(
-    "http://localhost:3000/api/agents/cly4g3c15000dc1wgy0h6sx74/openapi",
-    llm,
-    verbose=True
-)
+url = input("Enter a URL to summarize (either a webpage or a YouTube video): ")
 
-result = chain.run(
-    """What's the summary of this video? https://www.youtube.com/watch?v=upC8hjr2b4g&ab_channel=fern"""
-)
 
-print(result)
+if "youtube.com" in url:
+    youtube_summarizer_chain = get_openapi_chain(
+        "http://localhost:3000/api/agents/cly4g3c15000dc1wgy0h6sx74/openapi",
+        llm,
+        verbose=True
+    )
+
+    result = youtube_summarizer_chain(url)
+
+
+else:
+    prompt_template = """Write a summary of the following in 250-300 words:
+
+                        {text}
+
+                    """
+    prompt = PromptTemplate(template=prompt_template, input_variables=["text"])
+
+    summarize_chain = load_summarize_chain(
+        llm, chain_type="stuff", prompt=prompt)
+
+    loader = UnstructuredURLLoader(urls=[url], ssl_verify=False, headers={
+                                   "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"})
+    data = loader.load()
+
+    result = summarize_chain.run(data)
+
+
+prompt_template = """This JSON contains results from a summarization job. Parse it and return only the relevant summary, in a human-readable form.
+
+                        {result}
+
+                    """
+
+prompt = PromptTemplate(template=prompt_template,
+                        input_variables=["result"])
+
+result_parser_chain = prompt | llm | StrOutputParser()
+
+response = result_parser_chain.invoke({"result": result})
+
+print(response)

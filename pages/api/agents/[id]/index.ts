f88efc9eb67ee53ec7bf7e12e989b112178ba17c
Zeyu Zhang <39144422+zeyu2001@users.noreply.github.com>
2024-07-03 22:18:04 +0800

    feat: forwarding to agent

---
+++ pages/api/agents/[id]/index.ts
@@ -0,0 +1,31 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import DB from '@/util/db';
+import { buffer } from 'micro';
+import axios from 'axios';
+
+const db = new DB();
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { id } = req.query;
+  const agent = await db.getAgentById(id);
+  if (!agent) return res.status(404).end();
+
+  const url = agent.url;
+  const buf = await buffer(req);
+
+  // Forward the request to the agent.
+  const response = await axios({
+    method: req.method,
+    url,
+    headers: req.headers,
+    data: buf,
+  });
+
+  return res.status(200).json(agent);
+}
---
+++ pages/api/agents/[id]/openapi.ts
@@ -0,0 +1,16 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import DB from '@/util/db';
+
+const db = new DB();
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { id } = req.query;
+  if (req.method === 'GET') {
+    const agent = await db.getAgentById(id);
+    if (!agent) return res.status(404).end();
+    return res.status(200).json(agent.schema);
+  } else {
+    res.setHeader('Allow', 'GET');
+    res.status(405).end('Method Not Allowed');
+  }
+}
